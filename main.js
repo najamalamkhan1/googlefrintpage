6b509dc44f5cad23af5827360e135fb25b990739
Najam Alam Khan <najamalamkhan1321@gmail.com>
2022-11-26 23:04:06 +0500

    Clone google front page

---
+++ main.js
@@ -0,0 +1,27 @@
+// elements selection section
+let a = document.getElementsByClassName('container');
+let e = a[0].style;
+
+let b = document.getElementById('ids...');
+let c = document.getElementsByClassName('h');
+// let m = h[0].style; let n = h[1].style; let o = h[2].style; let p = h[3].style; let q = h[4].style; let r = h[5].style;
+
+let f = c[0].style; let g = c[1].style; let h = c[2].style; let i = c[3].style; let j = c[4].style; let k = c[5].style;
+
+let d = document.getElementsByClassName('headings');
+let l = d[0].style;
+
+// implement css through js
+
+e.fontFamily = "'Noto Sans', sans-serif"; e.display = "flex"; e.height = "60vh"; e.width = "20vw"; e.margin = "auto";
+
+// Logo selcetion and implemend css
+f.color = "#4285F4"; g.color = "#EA4335"; h.color = "#FBBC05"; i.color = "#4285F4"; j.color = "#34A853"; k.color = "#EA4335";
+
+l.display = "flex";
+l.marginLeft = "2%";
+
+d[0].style.display = "flex";
+
+// input section
+a[1].setAttributeNode = "input"; 
